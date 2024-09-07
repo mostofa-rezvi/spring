fd0f25583bc6ec07d02621de480820d7f8c1390c
mostofa-rezvi <mostofa.aminur.rezvi@gmail.com>
2024-09-07 19:04:33 +0600

    Add

---
+++ Class/demoEntity/src/main/java/com/rezvi/demoEntity/DemoEntityApplication.java
@@ -0,0 +1,13 @@
+package com.rezvi.demoEntity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoEntityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoEntityApplication.class, args);
+	}
+
+}
---
+++ Class/demoEntity/src/test/java/com/rezvi/demoEntity/DemoEntityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rezvi.demoEntity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoEntityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
